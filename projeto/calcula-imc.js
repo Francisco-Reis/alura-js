faa1228bc9b22f587551a8866e9c56f2083efacd
Francisco F. Reis <franciscofreis@gmail.com>
2015-08-28 23:50:59 -0300

    Terceira aula

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,13 +1,14 @@
-var paciente = {};
 
-var paciente.peso = 100;
-var paciente.altura = 1.8;
-var paciente.nome = "Francisco";
+var paciente = {
+    nome: document.getElementById('nome-1').textContent,
+    peso: document.getElementById('peso-1').textContent,
+    altura: document.getElementById('altura-1').textContent
+};
 
 if (paciente.altura > 0) {
    var imc = paciente.peso / (paciente.altura * paciente.altura);
-
    console.log(imc);
+   document.getElementById('imc-1').textContent = imc;
 } else {
    console.log("Altura precisa ser maior que zero");
 }
